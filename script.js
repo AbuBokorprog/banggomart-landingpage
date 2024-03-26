3b02bf72cf3e0f629378f2da4e5581c6da294825
AbuBokorprog <abubokor1066@gmail.com>
2024-03-26 17:52:50 +0600

    banner created

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
     searchModal.hide();
   });
 });
+
+$(".carousel").slick({
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  // arrows: true,
+  prevArrow:
+    '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+  nextArrow:
+    '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+  adaptiveHeight: true,
+  cssEase: "linear",
+});
