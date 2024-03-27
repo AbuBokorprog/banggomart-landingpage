12abea99397fce398a09532c5361999ff8e6133d
AbuBokorprog <abubokor1066@gmail.com>
2024-03-27 09:41:13 +0600

    hot products, new arrival and regular products template design created

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ $(".carousel").slick({
   autoplay: true,
   autoplaySpeed: 2000,
   fade: true,
-  // arrows: true,
   prevArrow:
     '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
   nextArrow:
@@ -35,31 +34,31 @@ $(".carousel").slick({
 });
 
 // category
-const categoryCarousel = document.querySelector(".category-carousel");
-fetch("/category.json")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+// const categoryCarousel = document.querySelector(".category-carousel");
+// fetch("/category.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
 
-    data?.map((item) => {
-      const card = document.createElement("div");
+//     data?.map((item) => {
+//       const card = document.createElement("div");
 
-      card.innerHTML = `
-      <div
-          class="card text-center mx-4 bg-light border-0"
-          style="width: 10rem"
-        >
-        <img src="${item.image}" class="card-img-top category-img" alt="...">
-        <div class="card-body">
-          <p class="card-text">${item.category}</p>
-          <p class="card-text">${item.total_items} items</p>
-        </div>
-        </div>
-        `;
-      categoryCarousel.appendChild(card);
-    });
-  })
-  .catch((error) => console.error("Error fetching data:", error));
+//       card.innerHTML = `
+//       <div
+//           class="card text-center mx-4 bg-light border-0"
+//           style="width: 10rem"
+//         >
+//         <img src="${item.image}" class="card-img-top category-img" alt="...">
+//         <div class="card-body">
+//           <p class="card-text">${item.category}</p>
+//           <p class="card-text">${item.total_items} items</p>
+//         </div>
+//         </div>
+//         `;
+//       categoryCarousel.appendChild(card);
+//     });
+//   })
+//   .catch((error) => console.error("Error fetching data:", error));
 
 $(".category-carousel").slick({
   infinite: true,
@@ -67,13 +66,52 @@ $(".category-carousel").slick({
   slidesToShow: 6,
   slidesToScroll: 1,
   arrows: false,
-
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 6,
         slidesToScroll: 1,
+        arrows: false,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+      },
+    },
+  ],
+});
+$(".hot-carousel").slick({
+  infinite: true,
+  speed: 400,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow:
+    '<button type="button" class="slick-product-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+  nextArrow:
+    '<button type="button" class="slick-product-next"><i class="fa-solid fa-chevron-right"></i></button>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 1,
+
         infinite: true,
       },
     },
